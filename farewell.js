b8f594baa9f483ce007217c3f082053d2034dd42
Adam Moran <pipboy.moran9@gmail.com>
2019-07-10 13:05:56 -0500

    checkpoint 5

---
+++ farewell.js
@@ -0,0 +1,15 @@
+exports.farewellEnglish = function () {
+    console.log('Bye');
+};
+
+exports.farewellJapanese = function () {
+    console.log('Bai bai');
+}
+
+exports.farewellSpanish = function () {
+    console.log('Adiós');
+}
+
+exports.farewellIcelandic = function () {
+    console.log('Vertu blessaður');
+}
---
+++ main.js
@@ -0,0 +1,18 @@
+const greetings = require("./greeting.js");
+const farewell = require("./farewell.js");
+
+greetings.greetingEnglish();
+
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+farewell.farewellEnglish();
+
+farewell.farewellJapanese();
+
+farewell.farewellSpanish();
+
+farewell.farewellIcelandic();
